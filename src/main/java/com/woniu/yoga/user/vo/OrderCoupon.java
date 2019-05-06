65a59d37a4e3849cb41142e6d5c621b834013842
xchguo <guochx@163.com>
2019-05-06 09:29:34 +0800

    5.6ordercoupon

---
+++ src/main/java/com/woniu/yoga/user/vo/OrderCoupon.java
@@ -0,0 +1,15 @@
+package com.woniu.yoga.user.vo;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author Administrator
+ * @Date 2019/5/5 15:59
+ * @Version 1.0
+ */
+@Data
+public class OrderCoupon {
+    private String orderId;
+    private Integer couponId;
+}
